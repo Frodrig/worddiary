d8bd09b63e6ac7bc69f1d5a6f934bb1232091c13
Fernando Rodríguez <frodrig76@gmail.com>
2013-03-19 19:26:33 +0100

    Primera versión, con código a sustituir, que es capaz de dibujar el cursor de manera manual en el punto siguiente donde tendrá que ponerse el siguiente carácter. También centra correctamente la palabra en pantalla una vez no se está en modo edición. Se ha usado un carácter centinela para poder conocer la bounding box donde se tendrá que ir poniendo el texto en estado inicial. De todas formas, el código es altamente optimizable.

--- Code/Word Diary/WDWordRepresentationView.m
+++ Code/Word Diary/WDWordRepresentationView.m
@@ -16,12 +16,7 @@ static CGFloat CURSOR_OPACITY = 0.1;
 @interface WDWordRepresentationView()
 
 @property (weak, nonatomic) IBOutlet UIView *drawTextBox;
-//@property (nonatomic) NSTimeInterval        timeFrom;
-//@property (nonatomic) NSTimeInterval        acumulateTime;
 @property (nonatomic) UIColor               *actualColorOfCursor;
-
-//- (void)updateAnimation;
--(CTLineRef)createCTLineRefAdjustedToFitWithContextRef:(CGContextRef)contextRef withText:(NSString *)text color:(UIColor *)color activeCursor:(BOOL)cursor;
 
 @end
 
@@ -33,8 +28,6 @@ static CGFloat CURSOR_OPACITY = 0.1;
 @synthesize drawTextBox         = drawTextBox_;
 @synthesize delegate            = delegate_;
 @synthesize dataSource          = dataSource_;
-//@synthesize timeFrom            = timeFrom_;
-//@synthesize acumulateTime       = acumulateTime_;
 @synthesize actualColorOfCursor = actualColorOfCursor_;
 @synthesize dayOfTheWeekLabel   = dayOfTheWeekLabel_;
 
@@ -129,77 +122,52 @@ static CGFloat CURSOR_OPACITY = 0.1;
     
     CGPoint startPointDraw = startWordDrawingPoint;
     CGPoint endPointDraw = CGPointMake(self.bounds.size.width, startPointDraw.y);
-    const CGFloat dashPattern[] = {2.0, 6.0};
-
-    CGContextSaveGState(contextRef);
     
+    // Linea de puntos
+    const CGFloat dashPattern[] = {2.0, 6.0};
+    CGContextSaveGState(contextRef);
     CGContextSetLineDash(contextRef, 0, dashPattern, 2);
     CGContextSetLineWidth(contextRef, 0.5);
     CGContextSetRGBStrokeColor(contextRef, 0, 0, 0, 0.8);
     CGContextMoveToPoint(contextRef, 0.0, startPointDraw.y);
     CGContextAddLineToPoint(contextRef, self.bounds.size.width, endPointDraw.y);
     CGContextStrokePath(contextRef);
-    
     CGContextRestoreGState(contextRef);
-      
+    
+    // nota: creamos un string con cursor como caracter de referencia a la hora de hallar los bounds
     NSString *wordText = [self.dataSource actualTextValueForWordRepresentationView:self];
-    BOOL writeModeActive = [self.dataSource isInWritingModeFoWordRepresentationView:self] && wordText.length > 0;
-    CTLineRef line = [self createCTLineRefAdjustedToFitWithContextRef:contextRef withText:wordText color:[UIColor blackColor] activeCursor:writeModeActive];
-
-    CGRect lineImageBounds = CTLineGetImageBounds(line, contextRef);
-    CGPoint fontDrawPoint = CGPointMake(writeModeActive ? self.drawTextBox.frame.origin.x : self.drawTextBox.frame.origin.x + (self.drawTextBox.bounds.size.width - lineImageBounds.size.width) / 2, startPointDraw.y);
-
-    CGContextSaveGState(contextRef);
-
-    CGContextSetTextPosition(contextRef, fontDrawPoint.x, fontDrawPoint.y);
-    CTLineDraw(line, contextRef);
-    CFRelease(line);
+    NSString *wordTextWithCursor = [wordText stringByAppendingString:@"|"];
+    BOOL cursorVisible = [self.dataSource isInWritingModeFoWordRepresentationView:self] || wordText.length == 0;
     
-    CGContextRestoreGState(contextRef);
-
-    
-    if (wordText.length == 0) { 
-        CTLineRef placeholderLine = [self createCTLineRefAdjustedToFitWithContextRef:contextRef withText:NSLocalizedString(@"TAG_PLACEHOLDER_WORD", @"") color:self.actualColorOfCursor activeCursor:NO];
-        //CGRect lineImageBounds = CTLineGetImageBounds(placeholderLine, contextRef);
-        CGPoint fontDrawPoint = CGPointMake(self.drawTextBox.frame.origin.x , startPointDraw.y);
-
-        CGContextSaveGState(contextRef);
-        CGContextSetTextPosition(contextRef, fontDrawPoint.x, fontDrawPoint.y);
-        CTLineDraw(placeholderLine, contextRef);
-        CFRelease(placeholderLine);
-        
-        CGContextRestoreGState(contextRef);
-    }
-    
-    [self setNeedsDisplay];
-}
-
--(CTLineRef)createCTLineRefAdjustedToFitWithContextRef:(CGContextRef)contextRef withText:(NSString *)text color:(UIColor *)color activeCursor:(BOOL)cursor
-{
-    NSString *textToWrite = cursor ? [text stringByAppendingString:@"|"] : text;
     NSString *familyFont = [self.dataSource actualFamilyFontForWordRepresentationView:self];
     CGFloat fontSize = 100;
     
     CTLineRef line = nil;
+    CGRect lineImageBoundsWithoutCursor = CGRectNull;
+    CGRect lineImageBounds;
     BOOL endFindingFontSize = NO;
     do {
         CTFontRef fontRef = CTFontCreateWithName((__bridge CFStringRef)familyFont, fontSize, NULL);
         NSDictionary *attrDictionary = [NSDictionary dictionaryWithObjectsAndKeys:(__bridge id)fontRef, (NSString *)kCTFontAttributeName,
-                                        color, (NSString *)NSForegroundColorAttributeName, nil];
+                                        [UIColor blackColor], (NSString *)NSForegroundColorAttributeName, nil];
         CFRelease(fontRef);
         
-        NSMutableAttributedString *attString = [[NSMutableAttributedString alloc] initWithString:textToWrite attributes:attrDictionary];
-        if (cursor) {
-            [attString addAttribute:(NSString *)kCTForegroundColorAttributeName value:self.actualColorOfCursor range:NSMakeRange(textToWrite.length - 1, 1)];
-            //CTFontRef cursorFont = CTFontCreateWithName((__bridge CFStringRef)@"Courier", fontSize, NULL);
-            //[attString addAttribute:(NSString *)kCTFontAttributeName value:(__bridge id)(cursorFont) range:NSMakeRange(textToWrite.length - 1, 1)];
-            //CFRelease(cursorFont);
-        }
+        NSMutableAttributedString *attString = [[NSMutableAttributedString alloc] initWithString:wordTextWithCursor attributes:attrDictionary];
+        //[attString addAttribute:(NSString *)kCTForegroundColorAttributeName value:[UIColor colorWithWhite:1.0 alpha:1.0] range:NSMakeRange(wordTextWithCursor.length - 1, 1)];
+        [attString addAttribute:(NSString *)kCTForegroundColorAttributeName value:[UIColor colorWithWhite:1.0 alpha:0.0] range:NSMakeRange(wordTextWithCursor.length - 1, 1)];
         
         line = CTLineCreateWithAttributedString((__bridge CFAttributedStringRef)(attString));
         
+        // Para centrar sin tener en cuenta el cursor 
+       // if (CGRectEqualToRect(lineImageBoundsWithoutCursor, CGRectNull)) {
+            NSAttributedString *attStringWithoutCursor = [[NSAttributedString alloc] initWithString:wordText attributes:attrDictionary];
+            CTLineRef lineWithoutCursor = CTLineCreateWithAttributedString((__bridge CFAttributedStringRef)(attStringWithoutCursor));
+            lineImageBoundsWithoutCursor = CTLineGetImageBounds(lineWithoutCursor, contextRef);
+            CFRelease(lineWithoutCursor);
+        //}
+        
         // Set text position and draw the line into the graphics context
-        CGRect lineImageBounds = CTLineGetImageBounds(line, contextRef);
+        lineImageBounds = CTLineGetImageBounds(line, contextRef);
         endFindingFontSize = self.drawTextBox.frame.origin.x + lineImageBounds.size.width + 40.0 < self.drawTextBox.frame.origin.x + self.drawTextBox.bounds.size.width;
         if (endFindingFontSize) {
             endFindingFontSize = lineImageBounds.size.height + 40.0 < self.drawTextBox.bounds.size.height;
@@ -209,8 +177,41 @@ static CGFloat CURSOR_OPACITY = 0.1;
             CFRelease(line);
         }
     } while (!endFindingFontSize);
+    
+    //CTLineRef line = [self createCTLineRefAdjustedToFitWithContextRef:contextRef withText:wordText color:[UIColor blackColor] activeCursor:writeModeActive];
+    CGPoint fontDrawPoint = CGPointMake(cursorVisible ? self.drawTextBox.frame.origin.x : self.drawTextBox.frame.origin.x + (self.drawTextBox.bounds.size.width - lineImageBoundsWithoutCursor.size.width) / 2, startPointDraw.y);
 
-    return line;
+    CGContextSaveGState(contextRef);
+
+    CGContextSetTextPosition(contextRef, fontDrawPoint.x, fontDrawPoint.y);
+    
+    if (cursorVisible) {
+        CGRect cursorBounds = CGRectMake(fontDrawPoint.x, fontDrawPoint.y, 0.0, lineImageBounds.size.height);
+        if (wordText.length > 0) {
+            CFArrayRef lineGlyphRuns = CTLineGetGlyphRuns(line);
+            CFIndex glyphRunsCount = CFArrayGetCount(lineGlyphRuns);
+            CTRunRef glyphRunRef = CFArrayGetValueAtIndex(lineGlyphRuns, glyphRunsCount - 2);
+            CGFloat xOffset = CTLineGetOffsetForStringIndex(line, CTRunGetStringRange(glyphRunRef).location + CTRunGetStringRange(glyphRunRef).length, NULL);
+            CGFloat width = CTRunGetTypographicBounds(glyphRunRef, CFRangeMake(0, 0), NULL, NULL, NULL);
+            cursorBounds = CGRectMake(fontDrawPoint.x + xOffset, cursorBounds.origin.y, width, cursorBounds.size.height);
+            //NSLog(@"WordTextLength %d glyphCount %ld xOffset %f width %f", wordText.length, glyphRunsCount, xOffset, width);
+        }
+        
+        CGContextSaveGState(contextRef);
+        CGContextSetLineWidth(contextRef, 1.0); 
+        CGContextSetStrokeColorWithColor(contextRef, self.actualColorOfCursor.CGColor);
+        CGContextMoveToPoint(contextRef, cursorBounds.origin.x, cursorBounds.origin.y + cursorBounds.size.height * 0.75);
+        CGContextAddLineToPoint(contextRef, cursorBounds.origin.x, cursorBounds.origin.y - cursorBounds.size.height * 0.25);
+        CGContextStrokePath(contextRef);
+        CGContextRestoreGState(contextRef);
+    }
+    
+    CTLineDraw(line, contextRef);
+    CFRelease(line);
+    
+    CGContextRestoreGState(contextRef);
+    
+    [self setNeedsDisplay];
 }
 
 #pragma mark - UIView sobrecarga
