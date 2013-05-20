4ddc5c2d3f96607c399e5c8834e942177f41dd89
Fernando Rodríguez <frodrig76@gmail.com>
2013-05-20 12:57:19 +0200

    Update final web. Por defecto en inglés.

--- Web/Output/Word_Diary/Word_Diary_(es)_files/Word_Diary_(es).js
+++ Web/Output/Word_Diary/Word_Diary_(es)_files/Word_Diary_(es).js
@@ -1,11 +1,11 @@
-// Created by iWeb 3.0.4 local-build-20130518
+// Created by iWeb 3.0.4 local-build-20130520
 
 setTransparentGifURL('Media/transparent.gif');function applyEffects()
 {var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWEmptyStroke()});registry.applyEffects();}
 function hostedOnDM()
 {return false;}
 function onPageLoad()
-{loadMozillaCSS('Word_Diary_-_English_files/Word_Diary_-_EnglishMoz.css')
+{loadMozillaCSS('Word_Diary_(es)_files/Word_Diary_(es)Moz.css')
 adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');detectBrowser();adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');adjustLineHeightIfTooBig('id6');adjustFontSizeIfTooBig('id6');Widget.onload();fixAllIEPNGs('Media/transparent.gif');applyEffects()}
 function onPageUnload()
 {Widget.onunload();}
